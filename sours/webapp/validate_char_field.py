dfc607701bd4aa6a265c1bb36597dfe086b5aa2d
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-08 02:48:19 +0600

    создал файл валидацию

--- sours/webapp/validate_char_field.py
+++ sours/webapp/validate_char_field.py
@@ -1,1 +1,15 @@
-def
+def task_validate(description, status, date_of_completion):
+    errors = {}
+    if not description:
+        errors['description'] = 'Обязательно'
+    elif len(description) > 60:
+        errors['description'] = 'превышает'
+
+    if not status:
+        errors['status'] = 'Обязательно'
+
+    if not date_of_completion:
+        errors['date_of_completion'] = 'Обязательно'
+
+    return errors
+
