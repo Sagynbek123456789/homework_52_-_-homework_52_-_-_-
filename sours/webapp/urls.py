5ad9b59b4b5fc85d85a874a72b2593f755f65f47
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-16 18:41:19 +0600

    закомитил дополниельные файлы

--- sours/webapp/urls.py
+++ sours/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import index_view, task_create_view, task_delete_view
+from webapp.views import index_view, task_create_view, task_delete_view, task_view
 
 urlpatterns = [
-    path('', index_view),
-    path('task/add/', task_create_view),
-    path('task/delete/', task_delete_view)
+    path('', index_view, name='index'),
+    path('task/add/', task_create_view, name='task_create'),
+    path('task/delete/', task_delete_view, name='task_delete'),
+    path('task/<int:pk>/', task_view, name='task_view')
 ]
